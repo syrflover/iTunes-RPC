aa3e4a9034cfecc73b78d47962bd3a725722d065
Lee Tae Woo <lee111107@gmail.com>
2019-04-23 22:38:23 +0900

    Update deleteAllAssets.ts

--- deleteAllAssets.ts
+++ deleteAllAssets.ts
@@ -1,10 +1,28 @@
+import * as fs from 'fs';
 import {
   deleteRichPresenceAsset,
   getRichPresenceAssets,
 } from './src/lib/discord';
 import env from './src/env';
 
-import remove from './src/lib/remove';
+const remove = async (p: string, deleteOnlyInDirectory: boolean = false) => {
+  const stat = await fs.promises.stat(p);
+
+  if (stat.isFile()) {
+    await fs.promises.unlink(p);
+  } else if (stat.isDirectory()) {
+    const readDir = await fs.promises.readdir(p);
+
+    if (readDir.length > 0) {
+      for (const s of readDir) {
+        await remove(`${p}/${s}`);
+      }
+    }
+    if (!deleteOnlyInDirectory) {
+      await fs.promises.rmdir(p);
+    }
+  }
+};
 
 remove(env.ASSET_FOLDER, true).then(() => {
   console.log('success remove all files and directory');
