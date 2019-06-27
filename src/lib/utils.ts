413c9f2896d3788751ff763c18c8c0c4796e7a32
Lee Tae Woo <syr@meu.works>
2019-06-28 06:12:21 +0900

    change random

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,4 +3,4 @@ import * as F from 'nodekell';
 export const uniq = <T>(f: (elem: T) => any, iter: F.Iter<T>) =>
 	F.run(iter, F.distinctBy(f), F.collect);
 
-export const random = <T>(a: T[]) => a[Math.floor(Math.random() * a.length)];
+export const random = <T>(a: T[]) => a[F.random(a.length)];
